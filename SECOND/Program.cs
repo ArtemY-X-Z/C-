4cd9eb2278e717c673241e47b73371f532001067
«Artem173» <foto173@mail.ru>
2023-01-28 06:27:04 +0300

    save

---
+++ SECOND/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введи число");
+string numberStr = Console.ReadLine();  // Всегда будет строкаs
+int number = Convert.ToInt32(numberStr);  //Из строчки получается число
+
+int result = number * number;
+
+Console.WriteLine($"Квадрат числа {number} = {result}");
