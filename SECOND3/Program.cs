b419908e8801ef36d25d694969a6e7331dc8fc10
«Artem173» <foto173@mail.ru>
2023-01-28 07:47:46 +0300

    Вывести из числа

---
+++ SECOND3/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число :");
+
+int number = Math.Abs(int.Parse(Console.ReadLine()));
+
+for (int i = -number; i <= number; i++)
+{
+    Console.Write($"{i} ");//интерполяция
+}
