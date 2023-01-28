09f46e090e5aba02d0bb0f596056efa9bdfcf1ef
«Artem173» <foto173@mail.ru>
2023-01-28 07:05:33 +0300

    Квадрат числа

---
+++ SECOND1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число А: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a/b == b)
+{
+  Console.WriteLine("Первое число является квадратом второго");  
+}
+else
+{
+    Console.WriteLine("Первое число не является квадратом второго");
+}
