bc5a1f80452d25e6bc94847cc38e476d1309e8b6
«Artem173» <foto173@mail.ru>
2023-01-28 07:24:09 +0300

    Календарь

---
+++ SECOND2_Kal/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Введите число от 1 до 7: ");
+string dayOfWeeks = Console.ReadLine();
+
+if (dayOfWeeks == "1")
+{
+    Console.WriteLine("Сегодня понедельник! ");
+}
+else if (dayOfWeeks == "2")
+{
+    Console.WriteLine("Сегодня Вторник! ");
+}
+else if (dayOfWeeks == "3")
+{
+    Console.WriteLine("Сегодня Среда ! ");
+}
+else if (dayOfWeeks == "4")
+{
+    Console.WriteLine("Сегодня Четверг! ");
+}
+else if (dayOfWeeks == "5")
+{
+    Console.WriteLine("Сегодня ПЯТНИЦА ! ");
+}
+else if (dayOfWeeks == "6")
+{
+    Console.WriteLine("Сегодня Суббота! ");
+}
+else if (dayOfWeeks == "7")
+{
+    Console.WriteLine("Сегодня Воскресенье! ");
+}
+else 
+{
+    Console.WriteLine("Вы ввели не верное число ! ");
+}
