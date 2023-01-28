d8b8e4e9b8940b183889f4a9fba2854986d09b24
«Artem173» <foto173@mail.ru>
2023-01-28 19:17:40 +0300

    Поиск числа

---
+++ Example011_/Program.cs
@@ -0,0 +1,20 @@
+﻿
+//             0  1  2    3   4   5  6   7
+int[] arrey = {1, 2, 33, 44, 65, 33, 2, 24 };
+
+int n = arrey.Length;
+int find = 33;
+
+int index = 0;
+
+while (index < n )
+{
+    if (arrey[index] == find)
+    {
+        Console.WriteLine(index);
+        break; // чтобы показывал только первое найденное 
+    }
+    //index = index + 1; вариант первый 
+    index++;
+
+}
