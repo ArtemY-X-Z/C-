5b87ce90bf8d8ad911d61207580e1ca68ec2ed40
«Artem173» <foto173@mail.ru>
2023-01-28 08:12:34 +0300

    Деление на 10 и остаток

---
+++ SEKOND4/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите число: ");
+string? numberStr = Console.ReadLine();
+if (numberStr != null){
+    int number = Math.Abs(int.Parse(numberStr));
+    Console.WriteLine(number%10);
+}
